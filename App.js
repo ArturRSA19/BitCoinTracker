4b3048789c1fede7264d923497a6e1ab08bd67fb
Artur Rodrigues <artur190503@outlook.com>
2023-12-04 15:08:10 -0300

    Iniciando app - 9:00 Aula 8 - Parte 1

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <StatusBar
+      backgroundColor="#00E6D8"
+      barStyle="dark-content"
+      translucent={true}
+      />
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 0,
+    marginTop: 100
   },
 });
